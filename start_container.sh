8e4cfc367ed97422306d9b9025d580bed5b8c86f
Siva Suribabu <108812647+SivaSuribabu@users.noreply.github.com>
2024-03-01 20:58:53 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sivasuribabu/sapmple-python-app
+docker pull sivasuribabu/sapmple-python-app:V1
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sivasuribabu/sapmple-python-app
+docker run -d -p 5000:5000 sivasuribabu/sapmple-python-app:V1
