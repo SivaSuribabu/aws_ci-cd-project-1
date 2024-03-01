524894da94b328fdf09aa09883e21bfd9c8b00ec
Siva Suribabu <108812647+SivaSuribabu@users.noreply.github.com>
2024-03-01 21:57:15 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -2,5 +2,6 @@
 set -e
 
 # Stop the running container (if any)
- ContainerId = "docker ps | awk -f " " '{print $1}' "
- docker rm -f $ContainerId
+#ContainerId = "docker ps | awk -f " " '{print $1}' "
+ #docker rm -f $ContainerId
+echo "Hi"
