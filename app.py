7e0c24109fb302d6bdd906b577b30006c7f658c5
Siva Suribabu <108812647+SivaSuribabu@users.noreply.github.com>
2024-03-05 00:02:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Siva Hello, world! This is my first aws codepipeline project'
+    return 'Siva Hello, world!'
 
 if __name__ == '__main__':
     app.run()
