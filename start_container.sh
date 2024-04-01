c0389efad7945d288665121014c4a076ab8d18b1
Siva Suribabu <108812647+SivaSuribabu@users.noreply.github.com>
2024-04-01 23:53:39 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull sivasuribabu/amazon-payments-prod:latest
+docker pull sivasuribabu/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 sivasuribabu/amazon-payments-prod:latest
+docker run -d -p 5000:5000 sivasuribabu/simple-python-flask-app:latest
